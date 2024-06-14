18869d0714a3e1a22d70e5f566c19dd106d0e561
Elena Gonzalez <elena.gonzalezlago@plexus.es>
2024-06-14 12:45:31 +0200

    build: persistencia en localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -18,6 +18,7 @@ export class GifsService {
   get tagsHistory() {
     return [...this._tagsHistory];
   }
+
   private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
 
@@ -27,8 +28,12 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
+private saveLocalStorage():void{
+  localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+}
 
   public searchTag(tag: string): void {
     if (tag.length === 0) return;
